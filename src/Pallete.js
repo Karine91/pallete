0b87542e2f435d9759949254ea99e494132d38dc
Karine91 <kitana.91@mail.ru>
2019-06-04 09:30:30 +0300

    init commit

---
+++ src/Pallete.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ColorBox from './ColorBox';
+import './Pallete.css';
+
+export class Pallete extends Component {
+    render() {
+        const colorBoxes = this.props.colors.map(color => (
+            <ColorBox key={color.color} background={color.color} name={color.name} />
+        ));
+        return (
+            <div className="Pallete">
+                <div className="Pallete-colors">{colorBoxes}</div>
+            </div>
+        );
+    }
+}
+
+export default Pallete;
